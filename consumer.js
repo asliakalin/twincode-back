6c8da5075b75be3def3c934c737c2467d7dd25d5
davbrican <72500941+davbrican@users.noreply.github.com>
2021-11-04 19:00:50 +0100

    Update consumer.js

--- consumer.js
+++ consumer.js
@@ -312,28 +312,7 @@ async function executeSession(sessionName, io) {
         });
         Logger.dbg(timer);
         timer--;
-      } else if ((session.testCounter == 0 && session.exerciseCounter == 0) || (session.testCounter == 0 && session.exerciseCounter == pairExercises.length / 2) || (session.testCounter == 1 && session.exerciseCounter == pairExercises.length / 2 + individualExercises.length) || (session.testCounter == 2 && session.exerciseCounter == pairExercises.length + individualExercises.length)) { //If timer goes to 0, and exercise in a test is the same as actual exercise, it goes to the next test
-        Logger.dbg("Going to the next test!");
-        //If exercises have been finished, it pass to a new test
-        Logger.dbg("Loading test");
-
-        var event = ["loadTest", {
-          data: {
-            testDescription: session.partsMessage[session.testCounter],
-            peerChange: true,
-          },
-        }];
-        io.to(sessionName).emit(event[0], event[1]);
-
-        lastSessionEvent.set(sessionName, event);
-        Logger.dbg("executeSession - lastSessionEvent saved", event[0]);
-
-
-        timer = 10; //Resets the timer
-        Logger.dbg("executeSession - testCounter: " + session.testCounter + " of " + 3 + " , exerciseCounter: " + session.exerciseCounter + " of " + maxExercises);
-
-        session.testCounter++;
-      } else { //If nothing before happens, it means that there are more exercises to do, and then in goes to the next one
+      } else if (false) { //If Users ask for a new exercise or finish the one the are doing
         Logger.dbg("Starting new exercise:");
         let testLanguage = session.language;
         let exercise =
@@ -368,7 +347,28 @@ async function executeSession(sessionName, io) {
         Logger.dbg("executeSession - session saved ");
 
         session.nextExercise = false;
-      }
+      } else if ((session.testCounter == 0 && session.exerciseCounter == 0) || (session.testCounter == 0 && session.exerciseCounter == pairExercises.length / 2) || (session.testCounter == 1 && session.exerciseCounter == pairExercises.length / 2 + individualExercises.length) || (session.testCounter == 2 && session.exerciseCounter == pairExercises.length + individualExercises.length)) { //If timer goes to 0, and exercise in a test is the same as actual exercise, it goes to the next test
+        Logger.dbg("Going to the next test!");
+        //If exercises have been finished, it pass to a new test
+        Logger.dbg("Loading test");
+
+        var event = ["loadTest", {
+          data: {
+            testDescription: session.partsMessage[session.testCounter],
+            peerChange: true,
+          },
+        }];
+        io.to(sessionName).emit(event[0], event[1]);
+
+        lastSessionEvent.set(sessionName, event);
+        Logger.dbg("executeSession - lastSessionEvent saved", event[0]);
+
+
+        timer = 10; //Resets the timer
+        Logger.dbg("executeSession - testCounter: " + session.testCounter + " of " + 3 + " , exerciseCounter: " + session.exerciseCounter + " of " + maxExercises);
+
+        session.testCounter++;
+      } 
 
       StandardSession.findOne({
         name: sessionName,
@@ -596,6 +596,12 @@ module.exports = {
         socket.id,
         "New user with socket id " + socket.id + " has entered"
       );
+
+      socket.on("resolvedExercise", (isExerciseCorrect) => {
+        Logger.dbg("EVENT resolvedExercise", isExerciseCorrect);
+        
+        socket.join(session);
+      });
 
       socket.on("adminConnected", (session) => {
         Logger.dbg("EVENT adminConnected", session);
